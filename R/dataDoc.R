d400e104c0325ecbb2ea2650b41a96a5f8174b13
Zack W. Almquist <almquist@umn.edu>
2015-08-27 16:03:19 -0500

    First Draft of lohrData package

---
+++ R/dataDoc.R
@@ -0,0 +1,249 @@
+#buildDatDoc<-function(dat){
+#for(i in dat){
+#  cat("#\'",i,"\n")
+#  cat("#\'","\n")
+#  cat("#\' A dataset from Lohr's Textbook on sampling\n")
+#  cat("#\'","\n")
+#  cat("#\' @format ",class(get(i)),"\n",sep="")
+#  cat("#\' @source Sharon Lohr (2010). \\emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.\n")
+#  cat("\"",i,"\"","\n",sep="")
+#  }
+#}
+#' agpop 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"agpop"
+#' agsrs 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"agsrs"
+#' agstrat 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"agstrat"
+#' anthrop 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"anthrop"
+#' anthsrs 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"anthsrs"
+#' anthuneq 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"anthuneq"
+#' audit 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"audit"
+#' books 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"books"
+#' certify 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"certify"
+#' coots 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"coots"
+#' counties 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"counties"
+#' divorce 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"divorce"
+#' dum1 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"dum1"
+#' golfsrs 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"golfsrs"
+#' htpop 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"htpop"
+#' htsrs 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"htsrs"
+#' htstrat 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"htstrat"
+#' journal 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"journal"
+#' measles 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"measles"
+#' ncvs 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"ncvs"
+#' nybight 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"nybight"
+#' otters 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"otters"
+#' ozone 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"ozone"
+#' samples 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"samples"
+#' seals 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"seals"
+#' selectrs 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"selectrs"
+#' statepop 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"statepop"
+#' statepps 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"statepps"
+#' syc 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"syc"
+#' teachers 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"teachers"
+#' teachmi 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"teachmi"
+#' teachnr 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"teachnr"
+#' uneqvar 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"uneqvar"
+#' winter 
+#' 
+#' A dataset from Lohr's Textbook on sampling
+#' 
+#' @format data.frame
+#' @source Sharon Lohr (2010). \emph{Sampling: Design and Analysis, 2nd Edition}, Duxbury Press.
+"winter"
